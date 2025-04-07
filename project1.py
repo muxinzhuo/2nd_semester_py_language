32147b27743b9d0d0a4af2ece018956d45d6e837
muxinzhuo <154701430+muxinzhuo@users.noreply.github.com>
2025-04-07 19:25:40 +0800

    Update project1.py

--- project1.py
+++ project1.py
@@ -9,6 +9,7 @@ print("世界那么大，{}想去{}看看。".format(name, country))
 # End
 
 # Begin
+name=input("输入姓名：")
 print("{}同学，学好Python，前途无量！".format(name))
 print("{}大侠，学好Python，大展拳脚！".format(name[0]))
 print("{}哥哥，学好Python，人见人爱！".format(name[1:]))
